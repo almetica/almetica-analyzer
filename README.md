dc23c9042c446aa4717f47633084d3fdf1247f78
Almetica <almetica@protonmail.com>
2020-05-17 12:53:05 +0200

    Initial commit.
    Just a straight port of the old codebase.

---
+++ README.md
@@ -0,0 +1,23 @@
+﻿# almetica-analyzer
+
+Simple fork of the analyzer code by Mirrawrs. Used to extract data needed by
+Almetica to serve as a custom server implementation for the MMORPG Tera.
+
+## Build
+
+Visual Studio 2019 (CE compatible)
+
+## Run
+
+```cmd
+TeraAnalyse.exe PATH_TO_TERA_EXE
+```
+
+The program will output three files:
+ - keys.yaml
+ - opcode.yaml
+ - messages.yaml
+
+You might need to retry to start the programm several times until it extracts
+the files successfully. You also most likely need to cleanup the opcode.yaml
+so that you only have the opcodes with the prefix "S_" and "C_".
---
+++ TeraAnalyse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tera.Analytics;
+
+namespace TeraAnalyse
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting TERA.exe");
+
+            var gamePath = @"D:\Tera Clients\TERA-93.04-EU\Binaries\TERA.exe";
+            var analyzer = new GameClientAnalyzer(gamePath);
+            var result = await analyzer.Analyze();
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"key.yaml"))
+            {
+                file.WriteLine("key: " + BitConverter.ToString(result.DataCenterKey).Replace("-", ""));
+                file.WriteLine("iv: " + BitConverter.ToString(result.DataCenterIv).Replace("-", ""));
+                file.Flush();
+                file.Close();
+            }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"opcode.yaml"))
+            {
+                foreach (KeyValuePair<ushort, string> packet in result.PacketNamesByOpcode)
+                {
+                    file.WriteLine(packet.Value + ": " + packet.Key);
+                }
+                file.Flush();
+                file.Close();
+            }
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"messages.yaml"))
+            {
+                UInt32 i = 0;
+                foreach (string msg in result.SystemMessageReadableIds)
+                {
+                    file.WriteLine("- " + msg);
+                    i++;
+                }
+                file.Flush();
+                file.Close();
+            }
+
+            Console.WriteLine("Finished extracting the data.");
+
+            return;
+        }
+    }
+}
